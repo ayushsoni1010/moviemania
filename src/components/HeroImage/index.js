ac67b773ebbda34d22ab371719ccf686f5641eab
Ayush Soni <mr.soni1002001@gmail.com>
2022-01-02 00:14:47 +0530

    feat: added hero section

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Styles
+import { Wrapper, Content, Text } from "./HeroImage.styles";
+
+const HeroImage = ({image , title , text}) => {
+  return (
+    <Wrapper image={image}>
+      <Content>
+        <Text>
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </Text>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default HeroImage;
