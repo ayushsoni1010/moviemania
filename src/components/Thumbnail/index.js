5d4f8ebd814e1796a69b31f62cfc9a15d9c6ffe5
Ayush Soni <mr.soni1002001@gmail.com>
2022-01-03 11:01:48 +0530

    wip: movie component and header link fixes

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // Styles
 import { Image } from "./Thumbnail.styles";
 
-const Thumbnail = ({image, movieId , clickable}) => {
-    return (
-        <React.Fragment>
-            <Image src={image} alt="movie-thumb"/>
-        </React.Fragment>
-    )
-}
+const Thumbnail = ({ image, movieId, clickable }) => {
+  return (
+    <React.Fragment>
+      {clickable ? (
+        <Link to={`/${movieId}`}>
+          <Image src={image} alt="movie-thumb" />
+        </Link>
+      ) : (
+        <Image src={image} alt="movie-thumb" />
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Thumbnail;
