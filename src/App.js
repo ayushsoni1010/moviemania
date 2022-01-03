b0b2850a8edeaa2a1868252d9b3ca72336389261
Ayush Soni <mr.soni1002001@gmail.com>
2022-01-03 10:39:12 +0530

    feat: added routes to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { GlobalStyle } from './styles/global/GlobalStyle';
+
+// Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Styles
+import { GlobalStyle } from "./styles/global/GlobalStyle";
+
+// Components
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
-function App() {
+const App = () => {
   return (
     <React.Fragment>
-      <GlobalStyle />
-      <Header />
-      <Home/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:movieId" element={<Movie />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <GlobalStyle />
+      </Router>
     </React.Fragment>
-    );
-}
+  );
+};
 
 export default App;
