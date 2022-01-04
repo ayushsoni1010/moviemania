19d5a248616b59c2c94395257348e6758b1c2216
Ayush Soni <mr.soni1002001@gmail.com>
2022-01-04 21:33:42 +0530

    feat: added movieInfo component

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 // Styles
 import { Image } from "./Thumbnail.styles";
 
-const Thumbnail = ({ image, movieId, clickable }) => {
+const Thumbnail = ({ image, movieId, clickable, movieHeight }) => {
   return (
     <React.Fragment>
       {clickable ? (
         <Link to={`/${movieId}`}>
-          <Image src={image} alt="movie-thumb" />
+          <Image src={image} alt="movie-thumb" movieHeight={movieHeight} />
         </Link>
       ) : (
-        <Image src={image} alt="movie-thumb" />
+        <Image src={image} alt="movie-thumb" movieHeight={movieHeight} />
       )}
     </React.Fragment>
   );
